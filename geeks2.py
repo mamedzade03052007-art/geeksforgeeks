256dc2818f0e1c6a29f8084c828385affe812925
Məhəmməd Məmmədzadə <s334842@karabakh.edu.az>
2025-12-05 03:25:22 +0400

    geeks2

---
+++ geeks2.py
@@ -0,0 +1,14 @@
+def leaders(arr):
+    res = []
+    n = len(arr)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[i] <= arr[j]:
+                break
+        else:
+            res.append(arr[i])
+    return res
+if __name__ == "__main__":
+    arr = [16, 17, 4, 3, 5, 2]
+    res = leaders(arr)
+    print(" ".join(map(str, res)))
